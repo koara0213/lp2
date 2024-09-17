c7be279241d9e93fe0c8784afeedcf422e36cd7b
koara0213 <mayu.koakoa.pearl@gmail.com>
2024-09-17 22:45:34 +0900

    初稿提出（コメントアウト削除）

--- lp2/assets/script/main.js
+++ lp2/assets/script/main.js
@@ -13,7 +13,6 @@ $(window).on('resize', function () {
   hamburger.removeClass('hamburger-menu-active');
 });
 
-// リンクを滑らかにスクロール
 $(document).ready(function(){
   $('a[href^="#"]').on('click', function(event) {
       var target = $(this.getAttribute('href'));
@@ -29,15 +28,14 @@ $(document).ready(function(){
 
 // お客様の声
 document.addEventListener('DOMContentLoaded', function () {
-  // slickスライダーの初期化
   $('.customer-slides').slick({
       infinite: true,
-      slidesToShow: 1, // 1枚のスライドを表示
-      slidesToScroll: 1, // 1枚ごとにスクロール
-      dots: true, // ナビゲーションのドット（ポチ点）を表示
-      arrows: true, // 左右のナビゲーションを表示
-      autoplay: false, // 自動再生をOFFに
-      autoplaySpeed: 2000, // 自動再生の速度（2000ミリ秒）
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: true,
+      autoplay: false,
+      autoplaySpeed: 2000,
       responsive: [{
           breakpoint: 768,
           settings: {
@@ -47,7 +45,6 @@ document.addEventListener('DOMContentLoaded', function () {
       }]
   });
 
-  // slickスライダーのイベントリスナーを設定
   $('.slider-prev').on('click', function() {
       $('.customer-slides').slick('slickPrev');
   });
@@ -58,15 +55,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// よくあるご質問のボックスがクリックされたら
+// Q&A
 $('.qa__description, .qa_containar_accordion-content_qa_question_content_accordion-icon').on('click', function() {
-  // .qa_containar_accordion-content_qa_answer_contentを選択
   var answerBox = $(this).closest('.qa_containar_accordion-content_qa').find('.qa_containar_accordion-content_qa_answer_content');
 
-  // .qa_containar_accordion-content_qa_answer_contentを表示・非表示
   answerBox.slideToggle();
 
-  // アイコンを切り替え
   var icon = $(this).closest('.qa_containar_accordion-content_qa').find('.bi-caret-down-fill, .bi-caret-up-fill');
 
   if (icon.hasClass('bi-caret-down-fill')) {
@@ -76,11 +70,9 @@ $('.qa__description, .qa_containar_accordion-content_qa_question_content_accordi
   }
 });
 
-// .qa__descriptionがクリックされたとき
 $('.qa__description').on('click', function(e) {
   var answerBox = $(this).closest('.qa_containar_accordion-content_qa').find('.qa_containar_accordion-content_qa_answer_content');
 
-  // display: flex に設定
   if (answerBox.css('display') === 'none') {
     answerBox.css('display', 'none');
   } else {
